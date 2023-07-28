fe0c0810467b790523ecfd8f9d0d936e87b2af3c
Prajul <prajulaadityaa@gmail.com>
2023-07-28 11:56:05 +0530

    testing precommit 10

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const data = [
   { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
   { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
   { category: "Fruits", price: "$3", stocked: true, name: "Pineapple" },
+  { category: "Fruits", price: "$5", stocked: false, name: "Strawberry" },
   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
